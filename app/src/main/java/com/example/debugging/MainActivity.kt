b365ceffd7be00998b65bc3aebbebb9d68361617
YuraKrulik <yurii.krulik@gmail.com>
2023-12-17 19:10:35 +0200

    Test debugging tools

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.debugging
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.thread
 
 private const val TAG = "MainActivity"
 
@@ -10,6 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "this is where the app crashed before")
+        val helloTextView: TextView = findViewById(R.id.division_textview)
+        Log.d(TAG, "this should be logged if the bug is fixed")
+        helloTextView.text = "Hello, debugging!"
         logging()
         division()
     }
@@ -22,12 +28,17 @@ class MainActivity : AppCompatActivity() {
         Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
 
-    fun division() {
+    private fun division() {
         val numerator = 60
         var denominator = 4
-        repeat(4) {
-            Log.v(TAG, "${numerator / denominator}")
-            denominator--
+        thread(start = true) {
+            repeat(4) {
+                Thread.sleep(3000)
+                runOnUiThread {
+                    findViewById<TextView>(R.id.division_textview).setText("${numerator / denominator}")
+                    denominator--
+                }
+            }
         }
     }
 }
